409781b037e928c08a11916258171854ba44829a
sibi-anand-vm <sibianand.vm2022ece@sece.ac.in>
2024-12-24 18:47:38 +0530

    commit after Separate page for Each Customer file

--- website/models.py
+++ website/models.py
@@ -1,3 +1,12 @@
 from django.db import models
-
-# Create your models here.
+class File(models.Model):
+    created_at=models.DateTimeField(auto_now_add=True)
+    username=models.CharField(max_length=50)
+    first_name=models.CharField(max_length=50)
+    last_name=models.CharField(max_length=50)
+    email=models.CharField(max_length=100)
+    address=models.CharField(max_length=150)
+    state=models.CharField(max_length=50)
+    pincode=models.IntegerField()
+    def __str__(self):
+        return(f"{self.username}")
--- website/views.py
+++ website/views.py
@@ -1,7 +1,11 @@
+import time
 from django.shortcuts import render,redirect
 from django.contrib.auth import authenticate,login,logout
 from django.contrib import messages
+from . forms import SignUpForm
+from .models import File
 def home(request):
+	files=File.objects.all()
 	if request.method=="POST":
 		username=request.POST['Username']
 		password=request.POST['Password']
@@ -14,10 +18,30 @@ def home(request):
 			messages.success(request,"There is some problem.Please Try again...")
 			return redirect('home')
 	else:	
-		return render(request,'home.html',{})
+		return render(request,'home.html',{'files':files})
 def logout_user(request):
 	logout(request)
 	messages.success(request,"You are logged out.")
 	return redirect('home')
 def register_user(request):
-	return render(request,'register.html',{})
+	if request.method=="POST":
+		form = SignUpForm(request.POST)
+		if form.is_valid():
+			form.save()
+			username=form.cleaned_data['username']
+			password=form.cleaned_data['password1']
+			user=authenticate(username=username,password=password)
+			login(request,user)
+			messages.success(request,"You have successfully registered")
+			return redirect('home')
+	else:
+		form=SignUpForm()
+		return render(request,'register.html',{'form':form})		
+	return render(request,'register.html',{'form':form})
+def customer_file(request,pk):
+	if request.user.is_authenticated:
+		cus_file=File.objects.get(id=pk)
+		return render(request,'file.html',{'cus_file':cus_file})
+	else:
+		messages.success(request,"You need to logged in")
+		return redirect('home')	
